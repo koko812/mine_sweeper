49ebfbae2dea727aa58d4c4e3efdc5dde892282e
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-07 20:20:41 +0900

    press_cells_and_count_bombs

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const width = 10
 const height = 10
 const size = 30
-const mineCount = 3
+const mineCount = 5
 
 const board = []
 for (let y = 0; y < height; y++) {
@@ -41,9 +41,73 @@ const init = () => {
             div.style.backgroundColor = `#ccc`
             div.style.border = `3px outset #eee`
             div.style.boxSizing = 'border-box'
+            div.style.fontSize = `${size * 0.6}px`
+            div.style.display = 'flex'
+            div.style.justifyContent = 'center'
+            div.style.alignItems = 'center'
+            //div.textContent = '1'
             container.appendChild(div)
+            board[y][x].element = div
+            div.onpointerdown = (e) => {
+                e.preventDefault()
+                open(x, y)
+            }
         }
     }
+}
+
+const update = () => {
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const cell = board[y][x]
+            if (cell.open) {
+                // #000 は黒くなりすぎ，元と同じ色だと，押した感が出てこない
+                cell.element.textContent = cell.text
+                cell.element.style.border = '1px solid #aaa'
+            }
+        }
+    }
+}
+
+const open = (x, y) => {
+    const cell = board[y][x]
+    console.log(x,y);
+    // 元から cell が空いている場合の処理を書き忘れていた
+    if (cell.open) {
+        return
+    }
+    cell.open = true
+
+    // これできれば全部の爆弾を爆発させたいかもしれない
+    if (cell.mine) {
+        cell.text = '💥'
+        update()
+        return
+    }
+
+    let counter = 0
+    for (let dy = -1; dy <= 1; dy++) {
+        for (let dx = -1; dx <= 1; dx++) {
+            // 書き方が微妙すぎた
+            //const cell = board[y+dy][x+dx]
+            //if(cell.mine){
+            //    counter++
+            //}
+            const cx = x + dx
+            const cy = y + dy
+            if (cx < 0 || cx > width || cy < 0 || cy > height) {
+                continue
+            }
+            if (board[cy][cx].mine) {
+                counter++
+            }
+        }
+    }
+
+    if (counter) {
+        cell.text = counter
+    }
+    update()
 }
 
 window.onload = () => {
